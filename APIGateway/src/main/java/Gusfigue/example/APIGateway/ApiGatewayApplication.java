1e4396ae597537b63a8c4b2882a3d907c1c2b314
Gus Figue <Gustavo.pereira1204@hotmail.com>
2026-04-07 23:11:36 -0300

    Add files via upload
    Configuração de API Gateway para roteamento de forma centralizada

---
+++ APIGateway/src/main/java/Gusfigue/example/APIGateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package Gusfigue.example.APIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ APIGateway/src/test/java/Gusfigue/example/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package Gusfigue.example.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
